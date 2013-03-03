10475ab5bc08cd5229a164625fc515bb1ef752b6
Mikkel Hoegh <mikkel@hoegh.org>
2013-03-03 18:06:50 +0000

    Removed broken code.
    We don't use the kolding theme, and we do not have a selectmenu.js.

--- template.php
+++ template.php
@@ -6,10 +6,9 @@
 function melsinore_ting_search_form($form){
   jquery_ui_add('ui.dialog');
   jquery_ui_theme_load();
-  drupal_add_js(drupal_get_path('theme', 'kolding').'/js/selectmenu.js', 'module', 'footer', TRUE);
 
-  $form['submit']['#type'] 	= "image_button" ;
-  $form['submit']['#src'] 	= drupal_get_path('theme','melsinore')."/images/search-btn.png";
+  $form['submit']['#type'] = "image_button" ;
+  $form['submit']['#src'] = drupal_get_path('theme','melsinore')."/images/search-btn.png";
   $form['submit']['#attributes']['class'] = "";
 
   unset($form['example_text']);
